04320b2c36eca61307ca4c6d1b14fa83b15af9c2
MianShang <napori03@naver.com>
2024-08-26 15:44:01 +0900

    day1 branch push

---
+++ JavaStudy2/src/Ex1/Test.java
@@ -0,0 +1,23 @@
+package Ex1;
+
+public class Test {
+    public static void main(String[] args) {
+        /*System.out.print("유한대학교\t");
+        System.out.println("소프트웨어공학과");
+        System.out.println("임지섭");
+
+        final int cc= 300;
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        *//*int cc = a * b;*//*
+        System.out.println(c);*/
+
+        int a= 7, b=9;
+
+        System.out.println(7+9);
+        System.out.println(a+b);
+        System.out.println("a+b = "+(a+b));
+        System.out.println(a + "+" +b +"="+(a+b));
+    }
+}
---
+++ JavaStudy2/src/test.java
@@ -0,0 +1,11 @@
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("안녕하세요");
+        System.out.println("Intelli J");
+        System.out.println("처음 써봐요");
+
+
+    }
+}
